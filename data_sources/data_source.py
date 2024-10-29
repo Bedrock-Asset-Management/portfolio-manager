acb682974426b263be63c2c4061d8e2488b435de
Ipse <www.ronan.c@gmail.com>
2024-10-29 15:01:41 +0100

    Add DataSource class

---
+++ data_sources/data_source.py
@@ -0,0 +1,14 @@
+# data_sources/data_source.py
+
+from abc import ABC, abstractmethod
+import pandas as pd
+
+class DataSource(ABC):
+    @abstractmethod
+    def fetch_data(self, tickers, start_date, end_date) -> pd.DataFrame:
+        """
+        Fetch historical data for specified tickers within the date range.
+        Returns:
+            DataFrame with price data.
+        """
+        pass
